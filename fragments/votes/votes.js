b2969570a0c562a40a063c76ceee36cc42ca5048
André Bittencourt <andr01a0@stud.kea.dk>
2021-12-21 05:02:41 +0100

    show votes

--- fragments/votes/votes.js
+++ fragments/votes/votes.js
@@ -1,12 +1,56 @@
 import renderHeader from "../../fragments/header/header.js"
 
 export default async () => {
-    renderHeader("nav-votes");
-    const main = document.querySelector(".main");
+  renderHeader("nav-votes");
+  const main = document.querySelector(".main");
 
-    const votesPageResponse = await fetch("./fragments/votes/votes.html");
-    const votesHtml = await votesPageResponse.text();
+  const votesPageResponse = await fetch("./fragments/votes/votes.html");
+  const votesHtml = await votesPageResponse.text();
 
-    main.innerHTML = votesHtml;
+  main.innerHTML = votesHtml;
 
+  const getCandidatesResponse = await fetch(`${window.apiUrl}/api/candidate`);
+  const candidates = await getCandidatesResponse.json();
+
+  const getPartiesResponse = await fetch(`${window.apiUrl}/api/party`);
+  const parties = await getPartiesResponse.json();
+
+  const data = [];
+  const backgroundColor = [];
+  const borderColor = [];
+  //Math.floor(Math.random() * 101);
+  parties.forEach(party => {
+    let votes = 0;
+    candidates.forEach(candidate => {
+      if(candidate.partyId == party.partyId) {
+        votes += candidate.votes;
+      }
+    });
+    data.push(votes);
+    const color = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, `;
+    backgroundColor.push(color + "0.2)");
+    backgroundColor.push(color + "1)");
+  });
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: parties.map(party => party.name),
+      datasets: [{
+        label: '# of Votes',
+        data: data,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 };
